1c4982f30ec5257953b2c2fe3fa58b0dc7da3084
halgurdh <halgurh@gmail.com>
2025-05-10 21:39:54 +0200

    fix prod

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  assetPrefix: isProd ? '/myBooks/' : undefined,
 };
 
 module.exports = nextConfig;
